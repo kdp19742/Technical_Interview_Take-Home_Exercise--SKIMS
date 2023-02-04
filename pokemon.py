4f9e43c0a9d68885d75b69ed9fe3a1dff9fe8d70
kdp19742 <keandparto@gmail.com>
2023-02-03 17:47:12 -0800

    clean up

--- pokemon.py
+++ pokemon.py
@@ -22,21 +22,21 @@ def get_pokemon_details():
     base_happiness = []
     for name, id in favorite_pokemon_dict.items():
         pokemon_details = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}').json()
-        pokemon_details_2 = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{id}/').json()
+        pokemon_species_details = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{id}/').json()
 
         # Keep base happiness of all 5 fav pokemon to calculate avg and median later
-        base_happiness.append(pokemon_details_2['base_happiness'])
-        
+        base_happiness.append(pokemon_species_details['base_happiness'])
+
         # Add the extracted attributes to the list of pokemons
         pokemons.append({
             'name': pokemon_details['name'],
             'height': pokemon_details['height'],
             'weight': pokemon_details['weight'],
-            'color': pokemon_details_2['color']['name'],
+            'color': pokemon_species_details['color']['name'],
             'moves': random.sample([move['move']['name'] for move in pokemon_details['moves']], 2),
-            'base_happiness': pokemon_details_2['base_happiness']
+            'base_happiness': pokemon_species_details['base_happiness']
         })
-    
+
     # Assume we are calculating the average and median base happiness of the 5 fav pokemon
     avg_base_happiness = statistics.mean(base_happiness)
     median_base_happiness = statistics.median(base_happiness)
